297ac8f5aab99e819fb6c290f60fcd51f08b5185
Graham Lea <graham@grahamlea.com>
2011-04-28 21:41:46 +1000

    Updated all copyright statements to reflect Apache Licencing

--- sodatest-runtime/src/test/scala/org/sodatest/runtime/processing/running/SodaFolderRunnerIntegrationTest.scala
+++ sodatest-runtime/src/test/scala/org/sodatest/runtime/processing/running/SodaFolderRunnerIntegrationTest.scala
@@ -1,4 +1,18 @@
-// Copyright (c) 2010 Belmont Technology Pty Ltd. All rights reserved.
+/*
+ * Copyright (c) 2010-2011 Belmont Technology Pty Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.sodatest.runtime.processing.running
 
@@ -60,7 +74,7 @@ class SodaFolderRunnerIntegrationTest extends XMLLoader[Elem] {
         case e: Elem => e.label match {
           case "style" => Text("")
           case "link" if (e.attribute("href") != None) => Text("")
-          case "p" if (e.attribute("class") == Some(Text("stackTrace"))) => firstTwoLinesOf(e)
+          case "p" if (e.attribute("class") == Some(Text("stackTrace"))) => shortCleanStackTrace(e)
           case _ => e.copy(child = e.child.map(f(_)))
         }
         case a => a
@@ -83,8 +97,8 @@ class SodaFolderRunnerIntegrationTest extends XMLLoader[Elem] {
     }))
   }
 
-  private def firstTwoLinesOf(e: Elem): Elem = {
-    e.copy(child = Text(firstTwoLinesOf(e.text)))
+  private def shortCleanStackTrace(e: Elem): Elem = {
+    e.copy(child = Text(firstTwoLinesOf(e.text.replaceAll(":[0-9]+\\)", ":)"))))  // Regex removes line numbers
   }
 
   private def firstTwoLinesOf(s: String): String = {
